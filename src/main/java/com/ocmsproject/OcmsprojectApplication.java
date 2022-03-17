aee846926ac8b002162adcc1e8dea386b262006f
JyotikaAgarwal <agarwaljyotika99@gmail.com>
2022-03-17 14:36:15 +0530

    My spring project

---
+++ src/main/java/com/ocmsproject/OcmsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ocmsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcmsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcmsprojectApplication.class, args);
+	}
+
+}
